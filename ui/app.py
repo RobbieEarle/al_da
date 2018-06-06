877422e699db7573232877bd47794ebb024cc22c
Robbie Earle <rb504035@dal.ca>
2018-06-06 14:10:10 -0300

    Started successfully sending messages between server and kiosk

--- ui/app.py
+++ ui/app.py
@@ -5,15 +5,22 @@ from flask_socketio import SocketIO, emit
 from helper.views import create_menu
 import eventlet
 
-i = 1
+eventlet.monkey_patch()
+
 my_thread = None
 output = ''
+
+new_socket_msg = False
+socket_msg = {'device_conn': {'active': False, 'url': '/static/images/scrape_no_conn.svg'},
+              'loading': {'active': False},
+              'done_loading': {'active': False}}
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'changeme123'
 app.debug = True
 socketio = SocketIO(app)
 CORS(app)
+scrape_refresh = False
 
 app.config.from_pyfile('config.py')
 
@@ -29,29 +36,68 @@ def index():
 
 
 @app.route('/scan')
-def submit():
+def scan():
     return render("scan.html", request.path)
 
 
-@socketio.on('to_kiosk')
-def test_connect(args):
-    global output
-    output = args
-
-
-@socketio.on('start', namespace='/output')
-def handle_message():
+@socketio.on('start')
+def start():
     global my_thread
-    emit('output', 'Connected', namespace='/output')
     if my_thread is None:
         my_thread = socketio.start_background_task(target=background_thread)
 
 
+@socketio.on('to_kiosk')
+def to_kiosk(args):
+    global output
+    output = args
+
+
+@socketio.on('device_event')
+def device_event(args):
+    global socket_msg
+    global new_socket_msg
+
+    print args
+
+    if args == 'connected':
+        socket_msg['device_conn']['active'] = True
+        socket_msg['device_conn']['url'] = '/static/images/scrape_conn.svg'
+        new_socket_msg = True
+    if args == 'disconnected':
+        socket_msg['device_conn']['active'] = True
+        socket_msg['device_conn']['url'] = '/static/images/scrape_no_conn.svg'
+        new_socket_msg = True
+    if args == 'loading':
+        socket_msg['loading']['active'] = True
+        new_socket_msg = True
+    if args == 'done_loading':
+        socket_msg['done_loading']['active'] = True
+        new_socket_msg = True
+
+
 def background_thread():
+    global output
+    global new_socket_msg
+    global socket_msg
+
     while True:
-        eventlet.sleep(0.1)
+        eventlet.sleep(0.01)
         with app.test_request_context():
-            socketio.emit('output', output, namespace='/output')
+            socketio.emit('output', output)
+
+        if new_socket_msg:
+            for msg in socket_msg:
+                if socket_msg[msg]['active'] is True:
+                    print msg
+                    if msg == 'device_conn':
+                        print socket_msg[msg]['url']
+                        socketio.emit(msg, socket_msg[msg]['url'])
+                    if msg == 'loading':
+                        print 'Successful load'
+                        socketio.emit(msg)
+                    socket_msg[msg]['active'] = False
+            new_socket_msg = False
 
 
 def render(template, path):
--- ui/static/js/socket_controller.js
+++ ui/static/js/socket_controller.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://' + document.domain + ':' + location.port + '/output');
+var socket = io.connect('http://' + document.domain + ':' + location.port);
     socket.on('connect', function() {
         socket.emit('start');
     });
@@ -6,3 +6,38 @@ var socket = io.connect('http://' + document.domain + ':' + location.port + '/ou
     socket.on('output', function(output_txt){
         document.getElementById("user_output_text").innerHTML = output_txt;
     });
+
+    socket.on('device_conn', function(img_url){
+        document.getElementById("user_output_img").src = img_url;
+    });
+
+    socket.on('loading', function(){
+        document.getElementById("user_output_img").src = '/static/images/ripple2.svg';
+    });
+
+    // socket.on('done_loading', function(){
+    // });
+
+    // socket.on('img_update', function(img_url, fade){
+    //     if (fade) {
+    //
+    //         document.getElementById("user_output_img").src = img_url;
+    //
+    //         // jQuery(function(){
+    //         //    // Fade Out
+    //         //    $("#user_output_img").fadeOut();
+    //         // });
+    //     }
+    //     else {
+    //         document.getElementById("user_output_img").src = img_url;
+    //     }
+    // });
+    //
+    // socket.on('loading_img', function(){
+    //     jQuery(function(){
+    //
+    //        // Fade Out
+    //        $("#user_output_img").fadeOut();
+    //
+    //     });
+    // });
