cf4299b448d257a9fbbdac448d35d4d19258c257
Robbie Earle <rb504035@dal.ca>
2018-08-07 11:44:26 -0300

    Working

--- ui/app.py
+++ ui/app.py
@@ -33,8 +33,8 @@ eventlet.monkey_patch()
 format_str = '%(asctime)s: %(levelname)s:\t %(message)s'
 date_format = '%Y-%m-%d %H:%M:%S'
 formatter = logging.Formatter(format_str, date_format)
-# local_handler = logging.handlers.RotatingFileHandler('/tmp/kiosk.log', maxBytes=500000, backupCount=5)
-local_handler = logging.handlers.RotatingFileHandler('C:/Users/Robert Earle/Desktop/al_device_audit/al_da/ui/kiosk.log', maxBytes=500000, backupCount=5)
+local_handler = logging.handlers.RotatingFileHandler('/tmp/kiosk.log', maxBytes=500000, backupCount=5)
+# local_handler = logging.handlers.RotatingFileHandler('C:/Users/Robert Earle/Desktop/al_device_audit/al_da/ui/kiosk.log', maxBytes=500000, backupCount=5)
 local_handler.setFormatter(formatter)
 
 my_logger = logging.getLogger("alda")
