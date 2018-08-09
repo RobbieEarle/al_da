47f8401a5dd98afd1b4899e22f2a0ea87bab3f29
Robbie Earle <rb504035@dal.ca>
2018-08-09 15:50:52 -0300

    Working

--- vbox_installer.py
+++ vbox_installer.py
@@ -33,6 +33,8 @@ def start():
     try:
         subprocess.call(['openssl', 'req', '-new', '-x509', '-newkey', 'rsa:2048', '-keyout', 'MOK.priv', '-outform',
                          'DER', '-out', 'MOK.der', '-nodes', '-days', '36500', '-subj', '"/CN=al_scrape/"'])
+        subprocess.call(['sudo', '/usr/src/linux-headers-$(uname -r)/scripts/sign-file', 'sha256', './MOK.priv',
+                         './MOK.der', '$(modinfo -n vboxdrv)'])
     except Exception as e:
         print "  Error signing kernel modules: " + str(e)
         print '    ' + str(type(e)) + ': ' + str(e)
