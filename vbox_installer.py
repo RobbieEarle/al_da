619e60ac0e1d2f08b6cae5590b124f0ff6001319
Robbie Earle <rb504035@dal.ca>
2018-08-09 15:41:17 -0300

    Working

--- vbox_installer.py
+++ vbox_installer.py
@@ -3,16 +3,40 @@ import subprocess
 
 def start():
     try:
-        # subprocess.call(['sudo', 'apt-get', 'install', 'virtualbox'])
-        subprocess.call(['sudo', 'apt-get', 'update'])
+        subprocess.call(['sudo', 'apt-get', 'install', 'virtualbox'])
     except Exception as e:
-        print "  Error installing VirtualBox: " + str(e)
+        print "  Error installing virtualbox: " + str(e)
         print '    ' + str(type(e)) + ': ' + str(e)
         exit(1)
 
-    # os.system('sudo apt-get install virtualbox-dkms')
-    # os.system('sudo apt-get install virtualbox-ext-pack')
-    # os.system('sudo apt-get install linux-headers generic')
+    try:
+        subprocess.call(['sudo', 'apt-get', 'install', 'virtualbox-dkms'])
+    except Exception as e:
+        print "  Error installing virtualbox-dkms: " + str(e)
+        print '    ' + str(type(e)) + ': ' + str(e)
+        exit(1)
+
+    try:
+        subprocess.call(['sudo', 'apt-get', 'install', 'virtualbox-ext-pack'])
+    except Exception as e:
+        print "  Error installing virtualbox-ext-pack: " + str(e)
+        print '    ' + str(type(e)) + ': ' + str(e)
+        exit(1)
+
+    try:
+        subprocess.call(['sudo', 'apt-get', 'install', 'linux-headers-generic'])
+    except Exception as e:
+        print "  Error installing generic linux-headers: " + str(e)
+        print '    ' + str(type(e)) + ': ' + str(e)
+        exit(1)
+
+    try:
+        subprocess.call(['openssl', 'req', '-new', '-x509', '-newkey', 'rsa:2048', '-keyout', 'MOK.priv', '-outform',
+                         'DER', '-out', 'MOK.der', '-nodes', '-days', '36500', '-subj', '"/CN=al_scrape/"'])
+    except Exception as e:
+        print "  Error signing kernel modules: " + str(e)
+        print '    ' + str(type(e)) + ': ' + str(e)
+        exit(1)
 
 
 if __name__ == '__main__':
