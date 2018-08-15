a838d36e0378e5aa84e25ad462e3b3a327d1230e
Robbie Earle <rb504035@dal.ca>
2018-08-15 15:07:10 -0300

    Working

--- ui/flask_service.py
+++ ui/flask_service.py
@@ -1,10 +1,10 @@
 import subprocess
 import time
 
-p = subprocess.Popen(['set', 'FLASK_APP=/home/user/al_da/ui/app.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+p = subprocess.Popen('set FLASK_APP=/home/user/al_da/ui/app.py', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                      shell=True, cwd='/home/user/al_da/ui/')
 
-p = subprocess.Popen(['flask', 'run'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+p = subprocess.Popen('flask run', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                      shell=True, cwd='/home/user/al_da/ui/')
 
 while True:
