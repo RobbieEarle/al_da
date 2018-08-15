5b7c3a1cf5b44b04bb7a14b351719b2c19cb39ba
Robbie Earle <rb504035@dal.ca>
2018-08-15 11:17:30 -0300

    Working

--- install/install_vbox.py
+++ install/install_vbox.py
@@ -9,40 +9,7 @@ def start():
 
     packages = ['virtualbox', 'virtualbox-dkms', 'virtualbox-ext-pack', 'linux-headers-generic']
     installer.sudo_apt_get_install(packages)
-
-    # try:
-    #     subprocess.call(['sudo', 'apt-get', 'install', 'virtualbox'])
-    # except Exception as e:
-    #     print "  Error installing virtualbox: " + str(e)
-    #     print '    ' + str(type(e)) + ': ' + str(e)
-    #     exit(1)
-    #
-    # try:
-    #     subprocess.call(['sudo', 'apt-get', 'install', 'virtualbox-dkms'])
-    # except Exception as e:
-    #     print "  Error installing virtualbox-dkms: " + str(e)
-    #     print '    ' + str(type(e)) + ': ' + str(e)
-    #     exit(1)
-    #
-    # try:
-    #     subprocess.call(['sudo', 'apt-get', 'install', 'virtualbox-ext-pack'])
-    # except Exception as e:
-    #     print "  Error installing virtualbox-ext-pack: " + str(e)
-    #     print '    ' + str(type(e)) + ': ' + str(e)
-    #     exit(1)
-    #
-    # try:
-    #     subprocess.call(['sudo', 'apt-get', 'install', 'linux-headers-generic'])
-    # except Exception as e:
-    #     print "  Error installing generic linux-headers: " + str(e)
-    #     print '    ' + str(type(e)) + ': ' + str(e)
-    #     exit(1)
-    #
-    # os.system('openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -days '
-    #           '36500 -subj "/CN=Descriptive common name/"')
-    # os.system('sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo '
-    #           '-n vboxdrv)')
-    # os.system('sudo mokutil --import MOK.der')
+    installer.sign_kernal_mods()
 
     # NEXT : Prompt user to restart their machine
 
