76ef1957151c25133a25aaa62562c241e43855dd
Robbie Earle <rb504035@dal.ca>
2018-08-15 16:39:07 -0300

    Working

--- ui/flask_service.py
+++ ui/flask_service.py
@@ -5,6 +5,7 @@ import re
 while True:
     active_connections = str(subprocess.check_output(['netstat', '-tuplen']))
     home_dir = str(subprocess.check_output('echo $HOME', shell=True))
+    print home_dir
 
     if not re.match('^[\s\S]*(127.0.0.1:5000)[^0][\s\S]*$', active_connections):
         p = subprocess.Popen('set FLASK_APP=' + home_dir + '/al_da/ui/app.py', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
