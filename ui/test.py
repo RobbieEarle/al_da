263714efe4e98fbd50ab619c45f3a98d482d03d8
Robbie Earle <rb504035@dal.ca>
2018-08-15 16:04:45 -0300

    Working

--- ui/test.py
+++ ui/test.py
@@ -5,7 +5,7 @@ active_connections = str(subprocess.check_output(['netstat', '-tuplen']))
 
 print active_connections
 
-if re.match('^.*(127.0.0.1:5000).*$', active_connections):
+if re.match('^[\s\S]*(127.0.0.1:5000)[^0][\s\S]*$', active_connections):
     print 'Success!'
 else:
     print 'Failure!'
