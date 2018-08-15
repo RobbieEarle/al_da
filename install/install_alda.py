0c28184a6ce224250a72b645c96bbc815596b67a
Robbie Earle <rb504035@dal.ca>
2018-08-15 18:21:07 -0300

    Working

--- install/install_alda.py
+++ install/install_alda.py
@@ -8,23 +8,25 @@ def start():
     if 'linux' in cur_os:
 
         installer = Installer('install_alda')
-        # installer.setup_logging(cur_os)
-        # installer.sudo_apt_get_install([
-        #     'python2.7',
-        #     'python-pip'
-        # ])
-        #
-        # installer.sudo_pip_install([
-        #     'flask==1.0.2',
-        #     'flask-socketio==3.0.1',
-        #     'flask-cors==3.0.6',
-        #     'eventlet==0.23.0',
-        #     'assemblyline-client==3.7.3',
-        #     'cryptography==2.3',
-        #     'email==4.0.2',
-        #     'flask-httpauth==3.2.4',
-        #     'arrow==0.12.1'
-        # ])
+
+        installer.setup_logging(cur_os)
+
+        installer.sudo_apt_get_install([
+            'python2.7',
+            'python-pip'
+        ])
+
+        installer.sudo_pip_install([
+            'flask==1.0.2',
+            'flask-socketio==3.0.1',
+            'flask-cors==3.0.6',
+            'eventlet==0.23.0',
+            'assemblyline-client==3.7.3',
+            'cryptography==2.3',
+            'email==4.0.2',
+            'flask-httpauth==3.2.4',
+            'arrow==0.12.1'
+        ])
 
         installer.make_service()
 
