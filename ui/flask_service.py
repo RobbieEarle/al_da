7ae1de79bbade8c6e57f0ba7f2e8871cc71c0d2c
Robbie Earle <rb504035@dal.ca>
2018-08-15 16:14:38 -0300

    Working

--- ui/flask_service.py
+++ ui/flask_service.py
@@ -1,13 +1,17 @@
 import subprocess
 import time
-
-p = subprocess.Popen('set FLASK_APP=/home/user/al_da/ui/app.py', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                     shell=True, cwd='/home/user/al_da/ui/')
-p.communicate()
-
-p = subprocess.Popen('flask run', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                     shell=True, cwd='/home/user/al_da/ui/')
-p.communicate()
+import re
 
 while True:
+    active_connections = str(subprocess.check_output(['netstat', '-tuplen']))
+
+    if not re.match('^[\s\S]*(127.0.0.1:5000)[^0][\s\S]*$', active_connections):
+        p = subprocess.Popen('set FLASK_APP=/home/user/al_da/ui/app.py', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                             shell=True, cwd='/home/user/al_da/ui/')
+        p.communicate()
+
+        p = subprocess.Popen('flask run', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                             shell=True, cwd='/home/user/al_da/ui/')
+        p.communicate()
+
     time.sleep(10)
