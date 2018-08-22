fe1fc8de4ae9469e09b7190a8162307029c17cd7
Robbie Earle <rb504035@dal.ca>
2018-08-22 13:54:35 -0300

    Working

--- ui/static/js/angular_controller.js
+++ ui/static/js/angular_controller.js
@@ -494,7 +494,7 @@ app.controller('ScanController', ['$scope', '$rootScope', function ScanControlle
                                             $rootScope.$emit("device_removed", {});
                                         })
                                     });
-                                }, 1500);
+                                }, 2000);
 
                             })
                         })
